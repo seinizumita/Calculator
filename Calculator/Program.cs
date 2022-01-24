a9959803f59a11c4c70fdbb963924c4b7e352b4a
Sein Izumita <sein.izumita@gmail.com>
2022-01-24 18:28:32 -0500

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = 9;
+int b = 10;
+int c = a + b;
+Console.WriteLine(c);
+Console.ReadKey();
